5ff8f92c193b6086fdfcb8a12d3d20b92f009ad0
Filipe Esperandio <filipesperandio@gmail.com>
2015-06-07 18:06:48 -0300

    login items

--- scripts/login.items.sh
+++ scripts/login.items.sh
@@ -1,7 +1,48 @@
 #!/usr/bin/env bash
-set -eu
+set -e
 
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/opt/homebrew-cask/Caskroom/dropbox/latest/Dropbox.app", name:"Dropbox"}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/opt/homebrew-cask/Caskroom/spectacle/0.8.10/Spectacle.app", name:"Spectacle"}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/opt/homebrew-cask/Caskroom/flux/latest/Flux.app", name:"Flux"}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/opt/homebrew-cask/Caskroom/caffeine/1.1.1/Caffeine.app", name:"Caffeine"}'
+start_on_login() {
+  local app=$1
+  local location=$(app_path $app)
+  local name=$(app_name $app)
+  local final_location=$([[ -n $name ]] && echo $location || app_from_application $app)
+
+  osascript -e "tell application \"System Events\" to make login item at end with properties {path:\"${final_location}\", name:\"${app}\", hidden: true}"
+}
+
+delete_all_login_items() {
+  for item in $(all_items)
+  do
+    osascript -e "tell application \"System Events\" to delete login item \"${item}\"" 
+  done
+}
+
+all_items() {
+  osascript -e 'tell application "System Events" to get the name of every login item' | tr ',' ' '
+}
+
+app_name() {
+  local app=$1
+  brew cask info  $app | grep '(app)' | awk '{print $1}'
+}
+
+app_from_application() {
+  local lookup_dir=/Applications
+  local app_name=$(ls $lookup_dir | grep -i $1)
+  echo "${lookup_dir}/${app_name}"
+}
+
+app_path() {
+  local app=$1
+  local dir=$(brew cask info $app | awk 'NR==4' | awk '{print $1}')
+  local app_name=$(app_name $app)
+  echo "${dir}/${app_name}"
+}
+
+delete_all_login_items
+
+start_on_login dropbox
+start_on_login spectacle
+start_on_login caffeine
+start_on_login shortcat
+start_on_login karabiner
