5692375262b585c656d80769802c6defeed1dfda
Filipe Esperandio <filipesperandio@gmail.com>
2015-06-07 19:10:47 -0300

    trackpad settings

---
+++ scripts/trackpad.defaults.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# To export trackpad settings
+# defaults export com.apple.AppleMultitouchTrackpad ~/.cider/scripts/trackpad.plist
+
+defaults import com.apple.AppleMultitouchTrackpad ~/.cider/scripts/trackpad.plist
+
