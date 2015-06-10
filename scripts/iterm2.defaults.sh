299669fea1b58000468b5b1e7a869951f81b10aa
Filipe Esperandio <filipesperandio@gmail.com>
2015-06-09 22:20:39 -0300

    add iterm settings

---
+++ scripts/iterm2.defaults.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# To export
+# defaults export ~/Library/Preferences/com.googlecode.iterm2.plist ./iterm.plist
+
+defaults import ~/Library/Preferences/com.googlecode.iterm2.plist ./iterm.plist
