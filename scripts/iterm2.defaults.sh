58017d740dcfaee8aba4fff4ce06105dfabd2ecb
Filipe Esperandio <filipesperandio@gmail.com>
2018-05-01 20:32:48 -0300

    Fix iterm defaults

--- scripts/iterm2.defaults.sh
+++ scripts/iterm2.defaults.sh
@@ -3,4 +3,4 @@
 # To export
 # defaults export ~/Library/Preferences/com.googlecode.iterm2.plist ./iterm.plist
 
-defaults import ~/Library/Preferences/com.googlecode.iterm2.plist ./iterm.plist
+defaults import ~/Library/Preferences/com.googlecode.iterm2.plist $(dirname ${BASH_SOURCE[0]})/iterm.plist
